429890594c93e5f9792dd336c447b31ee30d1d3e
AlpCanNalbant <enteldantel95@gmail.com>
2024-02-02 21:49:17 +0300

    The new String cpp source file is added for definitions of two non-inline functions.

---
+++ src/String.cpp
@@ -0,0 +1,38 @@
+// Copyright (c) Alp Can Nalbant. Licensed under the MIT License.
+
+#include "String.hpp"
+
+namespace Wcm
+{
+    bool UnorderedContainsT::operator()(const std::filesystem::path &lhs, const std::filesystem::path &rhs) const noexcept
+    {
+#ifdef WCM_CPP23
+        if (lhs.native().size() >= rhs.native().size())
+        {
+            return lhs.native().contains(rhs.native());
+        }
+        else
+        {
+            return rhs.native().contains(lhs.native());
+        }
+#else
+        if (lhs.native().size() >= rhs.native().size())
+        {
+            return lhs.native().find(rhs.native()) != std::string::npos;
+        }
+        else
+        {
+            return rhs.native().find(lhs.native()) != std::string::npos;
+        }
+#endif
+    }
+
+    bool ContainsT::operator()(const std::filesystem::path &lhs, const std::filesystem::path &rhs) const noexcept
+    {
+#ifdef WCM_CPP23
+        return lhs.native().contains(rhs.native());
+#else
+        return lhs.native().find(rhs.native()) != std::string::npos;
+#endif
+    }
+}
