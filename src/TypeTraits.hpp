fb31f146d3a79354a621093726b61f85648da97a
AlpCanNalbant <enteldantel95@gmail.com>
2024-02-20 21:12:10 +0300

    CountOf reduced to one line code. And GetCount function is removed.

--- src/TypeTraits.hpp
+++ src/TypeTraits.hpp
@@ -6,14 +6,6 @@
 
 namespace Wcm
 {
-    template <typename T, typename U = std::remove_cvref_t<std::remove_pointer_t<std::remove_extent_t<T>>>>
-    struct RemoveAllT : RemoveAllT<U> { };
-    template <typename T>
-    struct RemoveAllT<T, T>
-    {
-        using Type = T;
-    };
-
     template <typename... Types>
     struct IsAllSameT : std::true_type { };
     template <typename First, typename Second, typename ...Rest>
@@ -59,20 +51,6 @@ namespace Wcm
         requires (sizeof...(Types) >= Min && sizeof...(Types) <= Max)
     struct IsInRangeT<Min, Max, Types...> : std::true_type { };
 
-    // If value of the npos was used for when the type is not an array, it will not be error prone, even for the array of chars 'char arr[std::variant_npos]'.
-    template <typename T>
-    struct CountOfT : std::integral_constant<int, -1> { };
-    template <typename T, size_t Length>
-    struct CountOfT<T[Length]> : std::integral_constant<size_t, Length> { };
-    template <typename T, size_t Length>
-    struct CountOfT<const T[Length]> : std::integral_constant<size_t, Length> { };
-    template <typename T, size_t Length>
-    struct CountOfT<const T(&)[Length]> : std::integral_constant<size_t, Length> { };
-    template <typename T, size_t Length>
-    struct CountOfT<T(&&)[Length]> : std::integral_constant<size_t, Length> { };
-    template <typename T, size_t Length>
-    struct CountOfT<const T(&&)[Length]> : std::integral_constant<size_t, Length> { };
-
     template <size_t Index, typename... Types>
     using At = std::tuple_element_t<Index, std::tuple<Types...>>;
     template <typename... Types>
@@ -83,16 +61,9 @@ namespace Wcm
     using Third = At<2, Types...>;
     template <typename... Types>
     using Last = At<sizeof...(Types) - 1, Types...>;
-    template <typename T>
-    using RemoveAll = RemoveAllT<T>::Type;
-    template <typename T>
-    using ToPointer = std::add_pointer_t<std::remove_const_t<std::remove_pointer_t<T>>>;
-    template <typename T>
-    using ToConstPointer = std::add_pointer_t<std::add_const_t<std::remove_pointer_t<T>>>;
 
     template <typename T>
-    inline constexpr CountOfT<T>::value_type CountOf = CountOfT<T>::value;
-    consteval auto GetCount(auto &&arr) noexcept; // When you just don't want to use decltype.
+    inline constexpr std::conditional_t<Array<T>, size_t, int> CountOf = std::conditional_t<Array<T>, std::integral_constant<size_t, std::extent_v<std::remove_reference_t<T>>>, std::integral_constant<int, -1>>::value;
 
     template <typename... Types>
     inline constexpr bool IsAllSame = IsAllSameT<Types...>::value;
@@ -109,5 +80,3 @@ namespace Wcm
     template <size_t Min, size_t Max, typename... Types>
     inline constexpr bool IsInRange = IsInRangeT<Min, Max, Types...>::value;
 }
-
-#include "TypeTraits.inl"
